bbd950338479fd4da058e224a5b12a24cb831337
Evan Strat <evan10s@users.noreply.github.com>
2017-01-25 20:11:27 -0500

    Update code for B robot and create separate file for auton functions

---
+++ Claw Robots/autonomousLib.c
@@ -0,0 +1,65 @@
+//this file assumes that
+void setDumpMotors(int power) {
+	if (power > 127) {
+		power = 127;
+	} else if (power < -127) {
+		power = -127;
+	}
+	motor[lDump12] = power;
+	motor[lDump3] = power;
+	motor[rDump12] = power;
+	motor[rDump3] = power;
+}
+
+void setClawMotors(int power) {
+	motor[leftClaw] = power;
+	motor[rightClaw] = power;
+}
+
+void setLeftDtMotors(int power) {
+	motor[lDriveFront] = power;
+	motor[lDriveBack] = power;
+}
+
+void setRightDtMotors(int power) {
+	motor[rDriveFront] = power;
+	motor[rDriveBack] = power;
+}
+
+//2-motor forward right diagonal or backward left diagonal
+void diagonalLeft(int power, int dist) {
+	SensorValue[lDriveEnc] = 0;
+	while(abs(SensorValue[lDriveEnc]) < dist) {
+		writeDebugStreamLine("%d",SensorValue[lDriveEnc]);
+		motor[lDriveFront] = power;
+		motor[rDriveBack] = power;
+	}
+	if (power > 0) {
+		motor[lDriveFront] = -10;
+		motor[rDriveBack] = -10;
+	} else if (power < 0) {
+		motor[lDriveFront] = 10;
+		motor[rDriveBack] = 10;
+	}
+	wait1Msec(75);
+	motor[lDriveFront] = 0;
+	motor[rDriveBack] = 0;
+}
+
+void straight(int power, int dist) {
+	SensorValue[lDriveEnc] = 0;
+	while(abs(SensorValue[lDriveEnc]) < dist) {
+		setLeftDtMotors(power);
+		setRightDtMotors(power);
+	}
+	if (power > 0) {
+		setLeftDtMotors(-10);
+		setRightDtMotors(-10);
+	} else if (power < 0) {
+		setLeftDtMotors(10);
+		setRightDtMotors(10);
+	}
+	wait1Msec(75);
+	setLeftDtMotors(0);
+	setRightDtMotors(0);
+}
