2bb439bedfd6883d46e48e8e75ac03a04398348b
Evan Strat <evan10s@users.noreply.github.com>
2017-02-07 13:09:51 -0500

    Position PID and proportional straightening tuned

---
+++ State/Position PID - 4 Motor Struct.c
@@ -0,0 +1,279 @@
+
+float positionKp = .8, //proportional constant for positional PID
+			straighteningKpLeft = 0.29,//.43,//.195, //proportional constant for straightening response for the left side
+			straighteningKpRight = -0.29,//.22,//.16, //proportional constant for straightening response for the right side
+			straighteningKpLeftTurn = 0.4,//.43,//.195, //proportional constant for straightening response for the left side when turning
+			straighteningKpRightTurn = 0.4,//.22,//.16, //proportional constant for straightening response for the right side when turning
+			positionKi = 0.000350, //integral constant
+			positionKd = 4.25; //derivative constant
+
+
+void setLeftDtMotors(float power) {
+	motor[lDriveFront] = power;
+	motor[lDriveBack] = power;
+}
+
+void setRightDtMotors(float power) {
+	motor[rDriveFront] = power;
+	motor[rDriveBack] = power;
+}
+//Structure to hold all position PID related data
+typedef struct _position_pid_controller {
+    // Encoder
+    long            e_current;              ///< current encoder count
+    long            e_last;                 ///< last encoder count
+    // PID control algorithm variables
+    int            target;
+    int						 slewRateLimit;
+    float           error;                  ///
+    float           last_error;             ///< error last time update called
+    float						errorSum;
+    float						Kp;
+    float						Ki;
+    float						Kd;
+    float						p;
+    float						i;
+    float						d;
+
+    // final motor drive
+    long            motor_drive;            ///< final motor control value
+} posPID;
+
+static int STRAIGHT = 2; //the 2 here shouldn't matter as long as no variables are multiplied by 'direction' in driveDistancePID
+static int STRAFE = 3;
+static int ROTATE_LEFT = -1;
+static int ROTATE_RIGHT = 1;
+
+void posPidInit(posPID *pos, int target, float kP, float kI, float kD, float slewRate) {
+	pos->Kp = kP;
+	pos->Ki = kI;
+	pos->Kd = kD;
+	pos->target = target;
+	pos->last_error = 0;
+	pos->errorSum = 0;
+	pos->e_last = 0;
+	pos->slewRateLimit = slewRate;
+}
+
+//Check if motor power is > 127 or < -127, and if so, fix it
+float clampPower(float power) {
+	if (power > 127) {
+		return 127;
+	} else if (power < -127) {
+		return -127;
+	}
+	return power;
+}
+
+float posPidNextPower (posPID *pos,int encoderVal) {
+	pos->e_current = encoderVal;
+	pos->error = pos->target - pos->e_current;
+	pos->errorSum += pos->error;
+
+	int oldMotorDrive = pos->motor_drive;
+
+	pos->p = pos->error * (float) pos->Kp;
+	pos->i = pos->errorSum * (float) pos->Ki;
+	pos->d = (pos->error - pos->last_error) * (float)pos->Kd;
+
+	pos->last_error = pos->error;
+	float newMotorDrive = pos->p + pos->i + pos->d;
+
+	newMotorDrive = clampPower(newMotorDrive);
+
+	//apply a slew rate to limit acceleration/deceleration
+	if(abs(newMotorDrive-oldMotorDrive) > pos->slewRateLimit) {
+		if(newMotorDrive > oldMotorDrive) { //if the power is increasing (and the difference is greater than the slew rate allows)
+			newMotorDrive = oldMotorDrive + pos->slewRateLimit; //increment the power to only add
+		} else { //if the power is decreasing (and the difference is greater than the slew rate allows)
+			newMotorDrive = oldMotorDrive - pos->slewRateLimit;
+		}
+	}
+	pos->motor_drive = newMotorDrive;
+	//writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,pos->target,pos->error,SensorValue[lDriveEnc], SensorValue[rDriveEnc],pos->p,pos->i,pos->d,pos->motor_drive);
+	return newMotorDrive;
+}
+
+//@param time The maximum time in milliseconds to complete the operation
+void driveDistancePID(int encoderCounts, int direction, int time) {
+	int slewRateLimit = 15;
+	posPID drivePID;
+	posPidInit(drivePID,encoderCounts,positionKp,positionKi,positionKd,slewRateLimit);
+	writeDebugStreamLine("nPgmTime,target,error,SensorValue[lDriveEnc], SensorValue[rDriveEnc],pTerm,iTerm,dTerm,lPower,rPower");
+	//reset encoder values
+	SensorValue[lDriveEnc] = 0;
+	SensorValue[rDriveEnc] = 0;
+
+
+
+	time1[T1] = 0;
+	while(time1[T1] < time) {
+
+		if (direction == STRAIGHT) {
+			float encAverage = (SensorValue[lDriveEnc] + SensorValue[rDriveEnc])/2.0;
+			float power = posPidNextPower(drivePID,encAverage);
+
+			//adjust the powers sent to each side if the encoder values don't match
+			int straighteningError = SensorValue[lDriveEnc] - SensorValue[rDriveEnc];
+			float rPower, lPower;
+
+			writeDebugStreamLine("%f",straighteningError;
+			if (straighteningError > 0) { //left side is ahead, so speed up the right side
+				rPower = power + straighteningError*straighteningKpLeft;
+			} else { //otherwise, just set the right side to the power
+				rPower = power;
+			}
+			if (straighteningError < 0) { //right side is ahead, so speed up the left side
+				lPower = power - straighteningError*straighteningKpRight;
+			} else { //otherwise, just set the right side to the power
+				lPower = power;
+			}
+
+			writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,drivePID.target,drivePID.error,SensorValue[lDriveEnc], SensorValue[rDriveEnc],drivePID.p,drivePID.i,drivePID.d,lPower,rPower);
+			setLeftDtMotors(lPower);
+			setRightDtMotors(rPower);
+			wait1Msec(25);
+
+		} //end if (direction == STRAIGHT)
+		wait1Msec(25);
+
+		setLeftDtMotors(0);
+		setRightDtMotors(0);
+
+	} //end while time1[T1]
+
+//	if (direction == STRAIGHT) {
+//		while (time1[T1] < time) {
+//			//update error terms
+//			error = target - (nMotorEncoder[lDriveFront] + nMotorEncoder[rDriveFront])/2.0;
+
+
+//			pTerm = error * (float) positionKp;
+//			iTerm = errorSum * (float) positionKi;
+//			dTerm = (error - lastError) * (float) positionKd; //calculate motor power
+//			power = pTerm + iTerm + dTerm;
+
+//			//limit the values of the power term to only be those that can be taken by the motors
+//			if (power > 127) {
+//				power = 127;
+//			} else if (power < -127) {
+//				power = -127;
+//			}
+
+//			lastError = error; //update last error
+
+//			//apply a slew rate to limit acceleration/deceleration
+//			if(abs(power-lastPower) > slewRateLimit) {
+//				if(power > lastPower) { //if the power is increasing (and the difference is greater than the slew rate allows)
+//					power = lastPower + slewRateLimit; //increment the power to only add
+//				} else { //if the power is decreasing (and the differene is greater than the slew rate allows)
+//					power = lastPower - slewRateLimit;
+//				}
+//			}
+
+//			//adjust the powers sent to each side if the encoder values don't match
+//			straighteningError = nMotorEncoder[lDriveFront] - nMotorEncoder[rDriveFront];
+
+//			if (straighteningError > 0) { //left side is ahead, so speed up the right side
+//				rPower = power + straighteningError*straighteningKpLeft;
+//			} else { //otherwise, just set the right side to the power
+//				rPower = power;
+//			}
+//			if (straighteningError < 0) { //right side is ahead, so speed up the left side
+//				lPower = power - straighteningError*straighteningKpRight;
+//			} else { //otherwise, just set the right side to the power
+//				lPower = power;
+//			}
+
+//			lastPower = power; //update the last power
+//			writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,target,error,nMotorEncoder[lDriveFront], nMotorEncoder[rDriveFront],pTerm,iTerm,dTerm,lPower,rPower);
+//			setLDriveMotors(lPower);
+//			setRDriveMotors(rPower);
+//			wait1Msec(25);
+//		}
+//	} else if (direction == ROTATE_LEFT || direction == ROTATE_RIGHT) {
+//		while (time1[T1] < time) {
+//			//update error terms
+//			error = target - (abs(nMotorEncoder[lDriveFront]) + abs(nMotorEncoder[rDriveFront]))/2; //need to use absolute values here because one of
+//			errorSum += error;
+
+//			pTerm = error * (float) positionKp;
+//			iTerm = errorSum * (float) positionKi;
+//			dTerm = (error - lastError) * (float) positionKd; //calculate motor power
+//			power = pTerm + iTerm + dTerm;
+
+//			//limit the values of the power term to only be those that can be taken by the motors
+//			if (power > 127) {
+//				power = 127;
+//			} else if (power < -127) {
+//				power = -127;
+//			}
+
+//			lastError = error; //update last error
+
+//			//apply a slew rate to limit acceleration/deceleration
+//			if(abs(power-lastPower) > slewRateLimit) {
+//				if(power > lastPower) { //if the power is increasing (and the difference is greater than the slew rate allows)
+//					power = lastPower + slewRateLimit; //increment the power to only add
+//				} else { //if the power is decreasing (and the differene is greater than the slew rate allows)
+//					power = lastPower - slewRateLimit;
+//				}
+//			}
+
+//			//adjust the powers sent to each side if the encoder values don't match
+//			straighteningError = abs(nMotorEncoder[lDriveFront]) - abs(nMotorEncoder[rDriveFront]);
+
+//			if (straighteningError > 0) { //left side is ahead, so speed up the right side
+//				rPower = power + straighteningError*straighteningKpLeftTurn;
+//			} else { //otherwise, just set the right side to the power
+//				rPower = power;
+//			}
+//			if (straighteningError < 0) { //right side is ahead, so speed up the left side
+//				lPower = power - straighteningError*straighteningKpRightTurn;
+//			} else { //otherwise, just set the right side to the power
+//				lPower = power;
+//			}
+
+//			lastPower = power; //update the last power
+//			writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,error,nMotorEncoder[lDriveFront], nMotorEncoder[rDriveFront],pTerm,iTerm,dTerm,lPower,rPower);
+//			setLDriveMotors(lPower * direction); //for a left turn, ROTATE_LEFT = -1 so this moves the left side backwards for a left turn. For a right turn will go forward since ROTATE_RIGHT = 1
+//			setRDriveMotors(rPower * -1 * direction); //same idea as for a left turn, except this side needs to go the opposite way as the left side in order to turn, hence the * -1 in the calculation
+//			wait1Msec(25);
+//	}
+//}
+}
+
+//rotate the robot a certain amount
+//@param deg The number of degrees to turn; positive values are counterclockwise, negative values are clockwise.
+//@param direction The direction to turn in to get to the position; 1 is counterclockwise, -1 is clockwise
+//void rotateDegrees(int position, int direction) {//This function is for turning
+//	SensorValue[gyro] = 0;
+//	//Clear gyro
+//	if(direction == 1){
+//		//If direction == Left
+//		while(abs(SensorValue[gyro]) < position){
+//			//While the gyro is less than a set degrees, turn Left
+//			setRDriveMotors(45);
+//			setLDriveMotors(-45);
+//		}
+//		setRDriveMotors(-30);
+//		setLDriveMotors(30);
+//		wait1Msec(100); //brief brake
+//	}
+//	//end of LEFT turn
+//	else{
+//		//if direction == right
+//		while(abs(SensorValue[gyro]) < position){
+//			//While the gyro is less than a set degrees, turn right
+//			setRDriveMotors(-45);
+//			setLDriveMotors(45);
+//		}
+
+//		setRDriveMotors(30);
+//		setLDriveMotors(-30);
+//		wait1Msec(100); //brief brake
+//	} //end of RIGHT turn
+//	setRDriveMotors(0);
+//	setLDriveMotors(0);
+
+//}
---
+++ State/W Auton.c
@@ -0,0 +1,235 @@
+//START: global movement functions for basic robot subsystem movements
+void setDumpMotors(int power) {
+	if (power > 127) {
+		power = 127;
+		} else if (power < -127) {
+		power = -127;
+	}
+	motor[lDump12] = power;
+	motor[lDump3] = power;
+	motor[rDump12] = power;
+	motor[rDump3] = power;
+}
+
+void setClawMotors(int power) {
+	motor[leftClaw] = power;
+	motor[rightClaw] = power;
+}
+
+void setLeftDtMotors(int power) {
+	motor[lDriveFront] = power;
+	motor[lDriveBack] = power;
+}
+
+void setRightDtMotors(int power) {
+	motor[rDriveFront] = power;
+	motor[rDriveBack] = power;
+}
+//END: global functions
+
+//function to control claw compensation
+int target; //this variable needs global scope so PID can be controlled from outside this task.  Other variables are inside so they reset each time
+task clawCompensatePID()
+{
+	//some constants for claw PID
+	float kp = 0.5; //these constants might change down the road, but they are good for now
+	float ki = 0.01;
+	float kd = 0.00001;
+	int error;
+	int integral = 0;
+	int derivative;
+	int lastError = 0;
+	int PIDDrive;
+
+	while(1)
+	{
+			error = target - SensorValue[claw];
+			integral += error;
+			derivative = error - lastError;
+			PIDDrive = kp*error + ki*integral + kd*derivative;
+			setClawMotors(PIDDrive);
+			lastError = error;
+			wait1Msec(25); //don't hog the CPU
+	}
+}
+
+//2-motor forward right diagonal or backward left diagonal
+void diagonalLeft(int power, int dist) {
+	SensorValue[lDriveEnc] = 0;
+	while(abs(SensorValue[lDriveEnc]) < dist) {
+		writeDebugStreamLine("%d",SensorValue[lDriveEnc]);
+		motor[lDriveFront] = power;
+		motor[rDriveBack] = power;
+	}
+	if (power > 0) {
+		motor[lDriveFront] = -10;
+		motor[rDriveBack] = -10;
+	} else if (power < 0) {
+		motor[lDriveFront] = 10;
+		motor[rDriveBack] = 10;
+	}
+	wait1Msec(75);
+	motor[lDriveFront] = 0;
+	motor[rDriveBack] = 0;
+}
+
+void straight(int power, int dist) {
+	SensorValue[rDriveEnc] = 0;
+	while(abs(SensorValue[rDriveEnc]) < dist) {
+		setLeftDtMotors(power);
+		setRightDtMotors(power);
+	}
+	if (power > 0) {
+		setLeftDtMotors(-10);
+		setRightDtMotors(-10);
+	} else if (power < 0) {
+		setLeftDtMotors(10);
+		setRightDtMotors(10);
+	}
+	wait1Msec(75);
+	setLeftDtMotors(0);
+	setRightDtMotors(0);
+}
+void strafeRight(int target, int speed)
+{
+	SensorValue[rDriveEnc] = 0;
+	while(abs(SensorValue[rDriveEnc]) < target)
+	{
+		motor[lDriveFront] = speed;
+		motor[lDriveBack] = -speed;
+		motor[rDriveFront] = -speed;
+		motor[rDriveBack] = speed;
+	}
+	motor[lDriveFront] = 0;
+	motor[lDriveBack] = 0;
+	motor[rDriveFront] = 0;
+	motor[rDriveBack] = 0;
+}
+void strafeLeft(int target, int speed)
+{
+	SensorValue[rDriveEnc] = 0;
+	while(abs(SensorValue[rDriveEnc]) < target)
+	{
+		motor[lDriveFront] = speed; //this is actually the back wheel on the left side
+		motor[lDriveBack] = -speed;
+		motor[rDriveFront] = -speed;
+		motor[rDriveBack] = speed;
+	}
+	motor[lDriveFront] = 0;
+	motor[lDriveBack] = 0;
+	motor[rDriveFront] = 0;
+	motor[rDriveBack] = 0;
+}
+//high pot value = bottom
+//low pot value = top
+void liftToPotTarget(int target, int maxPower) {
+	int potStart = SensorValue[arm];
+	int difference = abs(target - potStart);
+	if (potStart > target && difference > 50) { //potentiometer value is above target.  New target is UP
+		while (SensorValue[arm] > target + 500 || SensorValue[arm] < target) {
+			setDumpMotors(abs(maxPower));
+		}
+		while (SensorValue[arm] > target || SensorValue[arm] < target - 50) {
+			setDumpMotors(abs(.3*maxPower));
+		}
+		setDumpMotors(-20);
+		wait1Msec(125);
+		setDumpMotors(0);
+	}
+	else if (potStart < target) { //potentiometer value is above target.  New target is UP
+		while (SensorValue[arm] < target - 500) {
+			setDumpMotors(-abs(maxPower));
+		}
+		while (SensorValue[arm] < target) {
+			setDumpMotors(-abs(.3*maxPower));
+		}
+		setDumpMotors(0);
+	}
+	setDumpMotors(12);
+
+}
+void killClawCompensation() // lets us turn off all compensation
+{
+	stopTask(clawCompensatePID);
+	setClawMotors(0);
+}
+
+void startClawCompensation(int newTarget) {
+	target = newTarget;
+	startTask(clawCompensatePID);
+}
+void manualCompensation() //allows us to use the single power compensation
+{
+	killClawCompensation(); //very important to do this before manually controlling the motors because PID is doing its thing
+	setClawMotors(15);
+}
+void moveClaw(int power, int potValue)//allows us to move the claw in auto and compensate
+{
+	killClawCompensation(); //very important to do this before manually controlling the motors because PID is doing its thing
+	if(SensorValue[claw] < potValue)
+	{
+		while(SensorValue[claw] < potValue)
+		{
+			setClawMotors(abs(power)); //if the claw needs to be opened, this makes sure you are using a positive power so that it doesn't try to move forever
+		}
+	}
+	else if(SensorValue[claw] > potValue)
+	{
+		while(SensorValue[claw] > potValue)
+		{
+			setClawMotors(-abs(power)); //the exact opposite of the above codition for positive input (credit to Evan for remembering the number -1 exists)
+		}
+	}
+	manualCompensation();
+	//startClawCompensation(potValue); //turns on the compensation code for the claw
+}
+
+
+void waitForLift(int target, int variance)
+{
+	while(SensorValue[arm] < target+variance && SensorValue[arm] > target-variance)
+	{
+		wait1Msec(25);
+	}
+}
+
+void waitForClaw(int target, int variance)
+{
+	while(SensorValue[claw] < target+variance && SensorValue[claw] > target-variance)
+	{
+		wait1Msec(25);
+	}
+}
+
+
+void rotateDegrees(int position, int direction) {//This function is for turning
+	SensorValue[gyro] = 0;
+	//Clear gyro
+	if(direction == 1){
+		//If direction == Left
+		while(abs(SensorValue[gyro]) < position){
+			//While the gyro is less than a set degrees, turn Left
+			setRightDtMotors(45);
+			setLeftDtMotors(-45);
+		}
+		setRightDtMotors(-30);
+		setLeftDtMotors(30);
+		wait1Msec(100); //brief brake
+	}
+	//end of LEFT turn
+	else{
+		//if direction == right
+		while(abs(SensorValue[gyro]) < position){
+			//While the gyro is less than a set degrees, turn right
+			setRightDtMotors(-45);
+			setLeftDtMotors(45);
+		}
+
+		setRightDtMotors(30);
+		setLeftDtMotors(-30);
+		wait1Msec(100); //brief brake
+	} //end of RIGHT turn
+	setRightDtMotors(0);
+	setLeftDtMotors(0);
+
+}
