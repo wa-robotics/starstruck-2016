de65c7fafa77dceea77fe5f920f779a36d96828b
Evan Strat <evan10s@users.noreply.github.com>
2017-03-31 08:13:49 -0400

    Updated drivetrain position PID to stop early when close to target and to support scaled encoder values

--- Worlds/Position PID - Tank v2.c
+++ Worlds/Position PID - Tank v2.c
@@ -1,11 +1,12 @@
 
-float positionKp = .8, //proportional constant for positional PID
+float positionKp = .7, //proportional constant for positional PID
 			straighteningKpLeft = 0.29,//.43,//.195, //proportional constant for straightening response for the left side
 			straighteningKpRight = 0.29,//.22,//.16, //proportional constant for straightening response for the right side
 			straighteningKpLeftTurn = 0.4,//.43,//.195, //proportional constant for straightening response for the left side when turning
 			straighteningKpRightTurn = 0.4,//.22,//.16, //proportional constant for straightening response for the right side when turning
 			positionKi = 0.000350, //integral constant
-			positionKd = 4.25; //derivative constant
+			positionKd = 4.25, //derivative constant
+			encoderScale = 0.6; //0.6 is 9:15.  This is a constant to multiply encoder values by, for example to scale down a high number
 
 
 static int STRAIGHT = 2; //the 2 here shouldn't matter as long as no variables are multiplied by 'direction' in driveDistancePID
@@ -15,7 +16,9 @@ static int STRAFE = 5;
 static int ROTATE_LEFT = -1;
 static int ROTATE_RIGHT = 1;
 
-int getScaledLeft
+float getScaledEncVal(float val) {
+	return val*encoderScale;
+}
 
 //Check if motor power is > 127 or < -127, and if so, fix it
 float clampPower(float power) {
@@ -27,21 +30,22 @@ float clampPower(float power) {
 	return power;
 }
 
+//time is the max time, the PID may stop earlier if it is close to target
 void liftToTargetPIDEnc(int target, int time, float kP, float kI, float kD) {
-	int error = 0,
-	errorSum = 0,
-	lastError = 0,
-	slewRateLimit = 15;
+	int slewRateLimit = 15;
 
 	float pTerm,
 	iTerm,
-	dTerm,
+	dTerm = 0,
 	power,
-	lastPower = 0;
+	lastPower = 0,
+	error = 0,
+	errorSum = 0,
+	lastError = 0;
 
 	time1[T1] = 0;
 
-	while (time1[T1] < time) {
+	while (time1[T1] < time || time1[T1] < 250 || abs(error) > abs(target*.1) || abs(dTerm) > 3) { //the PID can stop early if a little time as passed and the error is low and not changing much
 		//update error terms
 		error = target - SensorValue[liftEnc];
 		errorSum += error;
@@ -82,38 +86,41 @@ void liftToTargetPIDEnc(int target, int time, float kP, float kI, float kD) {
 //power is the power to run the motors at before straightening is applied
 //time is a maximum time to complete the operation
 void driveDistancePID(int encoderCounts, int direction, int time) {
-	writeDebugStreamLine("nPgmTime,target,error,nMotorEncoder[lDriveFront], nMotorEncoder[rDriveFront],pTerm,iTerm,dTerm,lPower,rPower");
+	writeDebugStreamLine("nPgmTime,target,error,lEncVal,rEncVal,pTerm,iTerm,dTerm,lPower,rPower");
 	//reset encoder values
 	Sensorvalue[lDriveEnc] = 0;
 	SensorValue[rDriveEnc] = 0;
 
-	int error = 0,
-	straighteningError = 0,
-	errorSum = 0,
-	lastError = 0,
-	target = encoderCounts,
-	slewRateLimit = 15,
-	lEncVal,
-	rEncVal;
+	int target = encoderCounts,
+	slewRateLimit = 15;
 
 	float pTerm,
 	iTerm,
-	dTerm,
+	dTerm = 0,
 	straighteningCorrection,
 	power,
 	lPower,
 	rPower,
-	lastPower = 0;
+	lastPower = 0,
+	error = 0,
+	straighteningError = 0,
+	errorSum = 0,
+	lastError = 0,
+	lEncVal = 0,
+	rEncVal = 0;
 
 	time1[T1] = 0;
 	if (direction == STRAIGHT || direction == ROTATE_LEFT || direction == ROTATE_RIGHT) { //validate direction
 
-			if (direction == STRAIGHT) {
-				while (time1[T1] < time) {
-					//update error terms
-					int lEncVal = getScaledLeftDtEncVal();
+	 if (direction == STRAIGHT) {
+		while ((time1[T1] < time) && (time1[T1] < 250 || abs(error) > 5 || abs(lEncVal - rEncVal) > 15 || abs(error - lastError) > 5)) { //the PID can stop early if a little time as passed and the error is low and not changing much
 
-					error = target - (SensorValue[lDriveEnc] + SensorValue[rDriveEnc])/2.0;
+					//get scaled encoder values
+					lEncVal = getScaledEncVal(SensorValue[lDriveEnc]);
+					rEncVal = getScaledEncVal(SensorValue[rDriveEnc]);
+
+					//update error terms
+					error = target - (lEncVal + rEncVal)/2.0;
 					errorSum += error;
 
 					pTerm = error * (float) positionKp;
@@ -140,7 +147,7 @@ void driveDistancePID(int encoderCounts, int direction, int time) {
 					}
 
 					//adjust the powers sent to each side if the encoder values don't match
-					straighteningError = abs(SensorValue[lDriveEnc]) - abs(SensorValue[rDriveEnc]);
+					straighteningError = abs(lEncVal) - abs(rEncVal);
 
 					if (straighteningError < 0) { //right side is ahead, so speed up the right side
 						rPower = power + straighteningError*straighteningKpRight;
@@ -154,17 +161,22 @@ void driveDistancePID(int encoderCounts, int direction, int time) {
 					}
 
 					lastPower = power; //update the last power
-					writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,target,error,SensorValue[lDriveEnc], SensorValue[rDriveEnc],pTerm,iTerm,dTerm,lPower,rPower);
+					writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,target,error,lEncVal,rEncVal,pTerm,iTerm,dTerm,lPower,rPower);
 					setLeftDtMotors(lPower);
 					setRightDtMotors(rPower);
 					wait1Msec(25);
 				}
+					writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,target,error,lEncVal,rEncVal,pTerm,iTerm,dTerm,lPower,rPower); //write to debug stream a final time so exit conditions for the while loop are visible
 					setLeftDtMotors(0);
 					setRightDtMotors(0);
 		 } else if (direction == ROTATE_LEFT || direction == ROTATE_RIGHT) {
-					while (time1[T1] < time) {
+					while ((time1[T1] < time) && (time1[T1] < 250 || abs(error) > 5 || abs(abs(lEncVal) - abs(rEncVal)) > 15 || abs(error - lastError) > 5)) { //the PID can stop early if a little time as passed and the error is low and not changing much
+						//get scaled encoder values
+						lEncVal = getScaledEncVal(SensorValue[lDriveEnc]);
+						rEncVal = getScaledEncVal(SensorValue[rDriveEnc]);
+
 						//update error terms
-						error = target - (abs(SensorValue[lDriveEnc]) + abs(SensorValue[rDriveEnc]))/2; //need to use absolute values here because one of
+						error = target - (abs(lEncVal) + abs(rEncVal))/2; //need to use absolute values here because one of
 						errorSum += error;
 
 						pTerm = error * (float) positionKp;
@@ -191,7 +203,7 @@ void driveDistancePID(int encoderCounts, int direction, int time) {
 						}
 
 						//adjust the powers sent to each side if the encoder values don't match
-						straighteningError = abs(SensorValue[lDriveEnc]) - abs(SensorValue[rDriveEnc]);
+						straighteningError = abs(lEncVal) - abs(rEncVal);
 
 						if (straighteningError > 0) { //left side is ahead, so speed up the right side
 							rPower = power + straighteningError*straighteningKpLeftTurn;
@@ -205,7 +217,7 @@ void driveDistancePID(int encoderCounts, int direction, int time) {
 						}
 
 						lastPower = power; //update the last power
-						writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,error,SensorValue[lDriveEnc],SensorValue[rDriveEnc],pTerm,iTerm,dTerm,lPower,rPower);
+						writeDebugStreamLine("%d,%f,%f,%f,%f,%f,%f,%f,%f",nPgmTime,error,lEncVal,rEncVal,pTerm,iTerm,dTerm,lPower,rPower);
 						setLeftDtMotors(lPower * direction); //for a left turn, ROTATE_LEFT = -1 so this moves the left side backwards for a left turn. For a right turn will go forward since ROTATE_RIGHT = 1
 						setRightDtMotors(rPower * -1 * direction); //same idea as for a left turn, except this side needs to go the opposite way as the left side in order to turn, hence the * -1 in the calculation
 						wait1Msec(25);
